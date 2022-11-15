58c6fb20522efeaec8a995b85ad46a01d5ad7d85
Heather <hnrogers10@gmail.com>
2022-11-15 14:51:51 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace SockPairs
+{
+    class Primary
+    {
+        public static void Main()
+        {
+
+            Working work = new Working();
+            Console.WriteLine(work.SockSorter("ABABC"));
+
+        }
+    }
+}
---
+++ SockPairs.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+namespace SockPairs
+{
+    class Working
+    {
+        public int SockSorter(string socks)
+        {
+            
+            return 1;
+        }
+    }
+}
