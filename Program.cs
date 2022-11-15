24c5a0485d15d0b46da22b03011bc0ea96d1fe70
Heather <hnrogers10@gmail.com>
2022-11-15 17:10:10 -0500

    Finished program Finished program

--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@ namespace SockPairs
     {
         public static void Main()
         {
-
             Working work = new Working();
+            
+            Console.WriteLine(work.SockSorter("AA"));
             Console.WriteLine(work.SockSorter("ABABC"));
-
+            Console.WriteLine(work.SockSorter("CABBACCC"));
+            Console.WriteLine(work.SockSorter(""));
         }
     }
 }
--- SockPairs.cs
+++ SockPairs.cs
@@ -7,8 +7,21 @@ namespace SockPairs
     {
         public int SockSorter(string socks)
         {
-            
-            return 1;
+
+            IDictionary<char, int> totalSocks = new Dictionary<char, int>();
+
+            for (int i = 0; i < socks.Length; i++)
+            {
+
+                if (!totalSocks.Keys.Contains(socks[i]))
+                {
+                    totalSocks.Add(socks[i], socks.Count(s => s == socks[i]));
+                }
+                
+            }
+
+            return totalSocks.Values.Where(s => s % 2 == 0).Sum() / 2;
+
         }
     }
 }
